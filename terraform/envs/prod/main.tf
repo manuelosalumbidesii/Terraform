82756fca7935e094233c0a7609645c25fcb98704
Manuelo Salumbides II <manuelosalumbidesii@gmail.com>
2025-09-28 18:47:03 +0800

    Fix Terraform formatting

--- terraform/envs/prod/main.tf
+++ terraform/envs/prod/main.tf
@@ -1,8 +1,8 @@
 module "vpc" {
-  source    = "../../modules/vpc"
-  vpc_cidr  = var.vpc_cidr
-  vpc_name  = var.vpc_name
-  env       = "prod"
+  source   = "../../modules/vpc"
+  vpc_cidr = var.vpc_cidr
+  vpc_name = var.vpc_name
+  env      = "prod"
 }
 variable "vpc_cidr" {
   type = string
--- terraform/modules/main.tf
+++ terraform/modules/main.tf
@@ -4,7 +4,7 @@ resource "aws_vpc" "main" {
   enable_dns_hostnames = true
 
   tags = {
-    Name = var.vpc_name
+    Name        = var.vpc_name
     Environment = var.env
   }
 }
