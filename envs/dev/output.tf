4f5880209c97a7fbc961df6faf1f6183e21e1b80
Manuelo Salumbides II <manuelosalumbidesii@gmail.com>
2025-09-30 10:57:20 +0800

    fi output

--- envs/dev/output.tf
+++ envs/dev/output.tf
@@ -1,11 +1,11 @@
 output "vpc_id" {
-  value = aws_vpc.main.id
+  value = module.network.vpc_id
 }
 
 output "subnet_id" {
-  value = aws_subnet.public.id
+  value = module.network.subnet_id
 }
 
 output "security_group_id" {
-  value = aws_security_group.web_sg.id
+  value = module.network.security_group_id
 }
