cc8f2f4a1e15427d94c82321ef41bf8a119ba016
Manuelo Salumbides II <manuelosalumbidesii@gmail.com>
2025-09-30 10:52:12 +0800

    fix format on provider

--- envs/dev/provider.tf
+++ envs/dev/provider.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "sample-tfstate-file"
-    key            = "envs/dev/terraform.tfstate"
-    region         = "us-east-2"
-    encrypt        = true
+    bucket  = "sample-tfstate-file"
+    key     = "envs/dev/terraform.tfstate"
+    region  = "us-east-2"
+    encrypt = true
   }
 }
 
