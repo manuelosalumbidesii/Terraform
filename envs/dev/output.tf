d4f53ee4ba4e17fce3d9395b73af6664d64122a3
Manuelo Salumbides II <manuelosalumbidesii@gmail.com>
2025-11-08 20:28:35 +0800

    added ec2

--- envs/dev/output.tf
+++ envs/dev/output.tf
@@ -3,25 +3,17 @@ output "vpc_id" {
 }
 
 output "subnet_id" {
-  value = module.network.subnet_id
+  value = module.network.public_subnet_id
 }
 
 output "security_group_id" {
-  value = module.network.security_group_id
+  value = module.network.default_sg_id
 }
 
 output "instance_id" {
-  value = aws_instance.this.id
+  value = module.ec2.instance_id
 }
 
 output "public_ip" {
-  value = aws_instance.this.public_ip
-}
-
-output "public_subnet_id" {
-  value = aws_subnet.public.id
-}
-
-output "default_sg_id" {
-  value = aws_security_group.web_sg.id
+  value = module.ec2.public_ip
 }
--- modules/ec2/output.tf
+++ modules/ec2/output.tf
@@ -1,9 +1,7 @@
 output "instance_id" {
-  description = "ID of the EC2 instance"
-  value       = aws_instance.this.id
+  value = aws_instance.this.id
 }
 
 output "public_ip" {
-  description = "Public IP of the EC2 instance"
-  value       = aws_instance.this.public_ip
+  value = aws_instance.this.public_ip
 }
